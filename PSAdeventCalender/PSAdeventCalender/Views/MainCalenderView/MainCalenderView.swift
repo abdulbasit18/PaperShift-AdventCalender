143a3195b1b23e2f8a8f8e0a37da6552acda5cb6
Abdul Basit <abdul.basit@veripark.com.pk>
2020-03-02 20:40:19 +0500

    Add main aalender view

---
+++ PSAdeventCalender/PSAdeventCalender/Views/MainCalenderView/MainCalenderView.swift
@@ -0,0 +1,21 @@
+//
+//  MainCalenderView.swift
+//  PSAdeventCalender
+//
+//  Created by Abdul Basit on 02/03/2020.
+//  Copyright © 2020 Abdul Basit. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainCalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainCalenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainCalenderView()
+    }
+}
