a0fd517f3cdbbb8af15568e870612ce9d1108bc8
Abdul Basit <abdul.basit@veripark.com.pk>
2020-03-02 20:32:37 +0500

    Extra assets removed

---
+++ PSAdeventCalender/PSAdeventCalender/Services/APIService.swift
@@ -0,0 +1,45 @@
+//
+//  APIService.swift
+//  PSAdeventCalender
+//
+//  Created by Abdul Basit on 02/03/2020.
+//  Copyright © 2020 Abdul Basit. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+protocol APIRequestType {
+    associatedtype Response: Decodable
+    var path: String { get }
+    var queryItems: [URLQueryItem]? { get }
+}
+
+protocol APIServiceType {
+    func response<Request>(from request: Request)
+        -> AnyPublisher<Request.Response, APIServiceError> where Request: APIRequestType
+}
+
+final class APIService: APIServiceType {
+    private let baseURL: URL
+    init(baseURL: URL = URL(string: ConfigurationConstants.baseURL)!) {
+        self.baseURL = baseURL
+    }
+    func response<Request>(from request: Request)
+        -> AnyPublisher<Request.Response, APIServiceError> where Request: APIRequestType {
+        let pathURL = URL(string: request.path, relativeTo: baseURL)!
+        var urlComponents = URLComponents(url: pathURL, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = request.queryItems
+        var request = URLRequest(url: urlComponents.url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let decorder = JSONDecoder()
+        decorder.keyDecodingStrategy = .convertFromSnakeCase
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map { data, _ in data }
+            .mapError { _ in APIServiceError.responseError }
+            .decode(type: Request.Response.self, decoder: decorder)
+            .mapError(APIServiceError.parseError)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
