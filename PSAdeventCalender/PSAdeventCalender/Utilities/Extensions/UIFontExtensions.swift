e8dac036493a1f484478f4d911b734f77f4b7d96
Abdul Basit <abdul.basit@veripark.com.pk>
2020-03-02 20:56:13 +0500

    Add font extentions file

---
+++ PSAdeventCalender/PSAdeventCalender/Utilities/Extensions/UIFontExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIFontExtensions.swift
+//  PSAdeventCalender
+//
+//  Created by Abdul Basit on 02/03/2020.
+//  Copyright © 2020 Abdul Basit. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static func appRegularFontWith(size: CGFloat ) -> Font {
+        return Font.custom("Lato-Regular", size: size)
+    }
+    static func appMediumFontWith(size: CGFloat ) -> Font {
+        return Font.custom("Lato-Medium", size: size)
+    }
+    static func appItalicFontWith(size: CGFloat ) -> Font {
+        return Font.custom("Lato-Italic", size: size)
+    }
+}
+
