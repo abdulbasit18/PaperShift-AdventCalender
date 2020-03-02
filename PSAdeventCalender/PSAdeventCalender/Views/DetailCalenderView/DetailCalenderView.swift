34a38ffdc19b2a319f4548ad63963a74ae84ffcf
Abdul Basit <abdul.basit@veripark.com.pk>
2020-03-02 20:41:13 +0500

    Add detail view

---
+++ PSAdeventCalender/PSAdeventCalender/Views/DetailCalenderView/DetailCalenderView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailCalenderView.swift
+//  PSAdeventCalender
+//
+//  Created by Abdul Basit on 02/03/2020.
+//  Copyright © 2020 Abdul Basit. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailCalenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailCalenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
