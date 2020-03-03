7abc282d208c5507c161e2227e4ff417d038150a
Abdul Basit <abdul.basit@veripark.com.pk>
2020-03-03 23:48:05 +0500

    Add Detail Screen

--- PSAdeventCalender/PSAdeventCalender/Views/DetailCalenderView/DetailCalenderView.swift
+++ PSAdeventCalender/PSAdeventCalender/Views/DetailCalenderView/DetailCalenderView.swift
@@ -9,7 +9,37 @@
 import SwiftUI
 
 struct DetailCalenderView: View {
+    
+    var calenderItem: AdventItemModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("papershift_logo")
+                .shadow(color: .primary, radius: 10, x: -1, y: 0.05)
+                .padding()
+//            Spacer()
+            
+            Image(calenderItem.image ?? "")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .shadow(color: .primary, radius: 5)
+                .frame(width: 200, height: 200, alignment: .top)
+                .padding()
+            
+            VStack {
+                Text(calenderItem.announcementTitle ?? "")
+                    .font(Font.appSemiBoldFontWith(size: 40))
+                    .foregroundColor(Color.primaryTextColor)
+                    .multilineTextAlignment(.center)
+                    .shadow(color: Color.primary, radius: 4, x: 0.5, y: 0.005)
+                Text(calenderItem.annouchmentMessage ?? "")
+                    .font(Font.appItalicFontWith(size: 25))
+                    .foregroundColor(Color.secondayTextColor)
+                    .multilineTextAlignment(.center)
+                .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+            }
+            Spacer()
+        }
     }
 }
